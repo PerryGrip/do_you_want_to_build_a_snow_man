6e1c94977a9c7a481f4499858401adca0f90e20b
PerryGrip <97786175+PerryGrip@users.noreply.github.com>
2022-05-19 13:40:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,15 +12,16 @@ var block_image_object= "";
 function new_image(get_image)
 {
 	// to upload images
-	fabric.Images.fromURL(get_image, function(Img) {
+	fabric.Image.fromURL(get_image, function(Img) {
+	block_image_object = Img;
 	block_image_object.scaleToWidth(block_image_width);
 	block_image_object.scaleToHeight(block_image_height);
 	block_image_object.set({
 top:block_y,
 left:block_x
-	})
+	});
 canvas.add(block_image_object);
-	})
+	});
 }
 
 window.addEventListener("keydown", my_keydown);
